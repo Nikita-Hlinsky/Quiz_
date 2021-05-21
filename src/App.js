05476d83a2dbd86dd7af46e425e8d617f8e8e8a4
Nikita-Hlinsky <super.nikita0308@gmail.com>
2021-05-21 17:07:34 +0300

    Init local rep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
 import './App.css';
 
 function App() {
+  const questions = [
+    {
+      questionText: '2>1?',
+      answerOptions: [
+        {answerText: 'yes', isCorrect: true},
+        {answerText: 'no', isCorrect: false}
+      ]
+    },
+    {
+      questionText: '3=3?',
+      answerOptions: [
+        {answerText: 'yes', isCorrect: true},
+        {answerText: 'no', isCorrect: false}
+      ]
+    },
+    {
+      questionText: '2+2?',
+      answerOptions: [
+        {answerText: '4', isCorrect: true},
+        {answerText: '6', isCorrect: false}
+      ]
+    },
+    {
+      questionText: '2*16?',
+      answerOptions: [
+        {answerText: '32', isCorrect: true},
+        {answerText: '24', isCorrect: false}
+      ]
+    },
+    {
+      questionText: '5/2?',
+      answerOptions: [
+        {answerText: '2.5', isCorrect: true},
+        {answerText: '1.2', isCorrect: false}
+      ]
+    },
+  ]
+
+  const [currentQuestion, setCurrentQuestion] = useState(0 )
+  const [score, setScore] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+
+  const handleQuestion = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+
+    const nextQuestion = currentQuestion + 1
+
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion)
+    } else {
+      setShowScore(true)
+    }
+  }
+
+  const refresh = () => {
+    setCurrentQuestion(0)
+    setScore(0)
+    setShowScore(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {
+        showScore
+          ? <div className="sectionScore">
+              <div>Correct answers {score} of {questions.length}</div>
+              <button
+                onClick={refresh}
+              >Play again</button>
+            </div>
+          : <div className="quiz">
+              <div className="question_section">
+                <div className="question_count">
+                  <span>Question {currentQuestion + 1}</span> of {questions.length}
+                </div>
+                <div className="question_text">{questions[currentQuestion].questionText}</div>
+              </div>
+              <div className="question_answer">
+                {questions[currentQuestion].answerOptions.map((question, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handleQuestion(question.isCorrect)}
+                  >{question.answerText}</button>
+                )
+                )}
+              </div>
+            </div>
+      }
     </div>
   );
 }
